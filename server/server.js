8261df2245598b999d8f112fc7a0f5912cf8de25
CharlyChaplin <charlychaplin83@gmail.com>
2023-09-13 15:26:51 +0700

    ...

--- server/server.js
+++ server/server.js
@@ -18,13 +18,7 @@ const app = express();
 var whitelist = ['https://lexun.ru'];
 
 var corsOptions = {
-	origin: function (origin, callback) {
-		if (whitelist.indexOf(origin) !== -1) {
-			callback(null, true)
-		} else {
-			callback(new Error('Not allowed by CORS'))
-		}
-	},
+	origin: 'https://lexun.ru',
 	// optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
 	credentials: true,
 	allowedHeaders: ['Content-Type', 'Authorization']
