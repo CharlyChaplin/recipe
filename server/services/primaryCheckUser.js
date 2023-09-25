827833805ffa0ab049ba61a7f64afc20abd53819
CharlyChaplin <charlychaplin83@gmail.com>
2023-09-25 12:44:42 +0700

    ...

--- server/services/primaryCheckUser.js
+++ server/services/primaryCheckUser.js
@@ -4,8 +4,8 @@ import TokenService from './tokenService.js';
 export async function primaryCheckUser({ accesstoken, refreshtoken }) {
 	if (!accesstoken && !refreshtoken) throw ApiError.UnathorizedError();
 	// проверяем accessToken на валидность
-	console.log(accesstoken);
 	const isAccessValid = TokenService.validAccessToken(accesstoken);
+	console.log(isAccessValid);
 	// проверяем refreshToken на валидность
 	const isRefreshValid = TokenService.validRefreshToken(refreshtoken);
 	// если токены валидны
