5054870f9e8bbda1ff64988f942234453e6b5a0f
CharlyChaplin <charlychaplin83@gmail.com>
2023-09-25 13:45:17 +0700

    ...

--- server/controllers/phraseController.js
+++ server/controllers/phraseController.js
@@ -7,99 +7,142 @@ import { primaryCheckUser } from '../services/primaryCheckUser.js';
 
 class PhraseController {
 	async addPhrase(req, res) {
-		const { accesstoken } = req.cookies;
-		if (!accesstoken) throw ApiError.UnathorizedError();
-		// проверяем accessToken на валидность
-		const isAccessValid = TokenService.validAccessToken(accesstoken);
-		// если токен валиден
-		let user_id = 0;
-		if (isAccessValid) {
+		try {
+			const { isAccessValid } = await primaryCheckUser(req.cookies);
+			if (!isAccessValid.email) throw ApiError.UnathorizedError();
+
 			// получаем id юзера, по email из токена
-			const getUser = await db.query(`
+			const checkGetUser = await db.query(`
 				SELECT * FROM users
 				WHERE email = '${isAccessValid.email}';
 			`);
-			if (!getUser.rowCount) throw ApiError.UnathorizedError();
-			user_id = getUser.rows[0].id;
-		}
 
-		// после всех проверок достаём фразу для занесения её в БД
-		const { phraseText } = req.body;
-		db.query(`SELECT MAX(id) FROM phrase;`)
-			.then(resp => db.query(`ALTER SEQUENCE phrase_id_seq RESTART WITH ${resp.rows[0].max + 1};`));
-		db.query(`INSERT INTO phrase(user_id, caption) VALUES (
-			'${user_id}','${phraseText}') RETURNING *;`)
-			.then(resp => res.json(resp.rows[0]))
-			.catch(err => {
-				if (Number(err.code) === 23505) {
-					res.status(400).json({ message: "Такая фраза уже существует!" });
-				} else {
-					res.status(400).json({ message: "Ошибка при добавлении" });
-				}
-			});
+			if (!checkGetUser.rowCount) throw ApiError.UnathorizedError();
+			const role = checkGetUser.rows[0].role;
+			const roleDescription = role === 1 ? 'admin' : role === 2 ? 'user' : 'unknown';
+
+			if (roleDescription === 'admin') {
+				// после всех проверок достаём фразу для занесения её в БД
+				const { phraseText } = req.body;
+				// сбрасываем счётчик последовательности в таблице phrase
+				await ResetSeq.resetSequence('phrase');
+				// добавляем фразу
+				db.query(`
+					INSERT INTO phrase(user_id, caption)
+					VALUES ('${user_id}','${phraseText}')
+					RETURNING *;
+				`)
+					.then(resp => res.json(resp.rows[0]))
+					.catch(err => {
+						if (Number(err.code) === 23505) {
+							res.status(400).json({ message: "Такая фраза уже существует!" });
+						} else {
+							res.status(400).json({ message: "Ошибка при добавлении" });
+						}
+					});
+			}
+
+		} catch (err) {
+			res.status(400).json({ message: err });
+		}
 	}
 
 	async deletePhrase(req, res) {
-		const { accesstoken } = req.cookies;
-		if (!accesstoken) throw ApiError.UnathorizedError();
-		// проверяем accessToken на валидность
-		const isAccessValid = TokenService.validAccessToken(accesstoken);
-		// если токен валиден
-		let user_id = 0;
-		if (isAccessValid) {
+		try {
+			const { isAccessValid } = await primaryCheckUser(req.cookies);
+			if (!isAccessValid.email) throw ApiError.UnathorizedError();
+
 			// получаем id юзера, по email из токена
-			const getUser = await db.query(`
+			const checkGetUser = await db.query(`
 				SELECT * FROM users
 				WHERE email = '${isAccessValid.email}';
 			`);
-			if (!getUser.rowCount) throw ApiError.UnathorizedError();
-			user_id = getUser.rows[0].id;
+
+			if (!checkGetUser.rowCount) throw ApiError.UnathorizedError();
+			const role = checkGetUser.rows[0].role;
+			const roleDescription = role === 1 ? 'admin' : role === 2 ? 'user' : 'unknown';
+
+			if (roleDescription === 'admin') {
+				// после всех проверок достаём фразу для удаления её из БД
+				const { phrase } = req.body;
+				db.query(`DELETE FROM phrase WHERE caption='${phrase}' RETURNING caption;`)
+					.then(resp => res.json([resp.rows[0].caption]))
+					.catch(err => res.status(400).json({ message: err }));
+			}
+		} catch (err) {
+			res.status(400).json({ message: err });
 		}
-		// после всех проверок достаём фразу для удаления её из БД
-		const { phrase } = req.body;
-		db.query(`DELETE FROM phrase WHERE caption='${phrase}' RETURNING caption;`)
-			.then(resp => res.json([resp.rows[0].caption]))
-			.catch(err => res.json({ message: err }));
 	}
 
 	async changePhrase(req, res) {
-		const { isAccessValid } = await primaryCheckUser(req.cookies);
-		if (!isAccessValid.email) throw ApiError.UnathorizedError();
+		try {
+			const { isAccessValid } = await primaryCheckUser(req.cookies);
+			if (!isAccessValid.email) throw ApiError.UnathorizedError();
 
-		// если токен валиден
-		let user_id = 0;
-		if (isAccessValid) {
 			// получаем id юзера, по email из токена
-			const getUser = await db.query(`
+			const checkGetUser = await db.query(`
 				SELECT * FROM users
 				WHERE email = '${isAccessValid.email}';
 			`);
-			if (!getUser.rowCount) throw ApiError.UnathorizedError();
-			user_id = getUser.rows[0].id;
+
+			if (!checkGetUser.rowCount) throw ApiError.UnathorizedError();
+			const role = checkGetUser.rows[0].role;
+			const roleDescription = role === 1 ? 'admin' : role === 2 ? 'user' : 'unknown';
+
+			if (roleDescription === 'admin') {
+				// после всех проверок достаём фразы для изменения в БД
+				const { oldPhrase, newPhrase } = req.body;
+
+				// проверяем есть ли такая фраза
+				const isExistsPhrase = await db.query(`
+					SELECT id FROM phrase
+					WHERE caption = '${oldPhrase}';
+				`);
+				if (!isExistsPhrase.rowCount) throw ApiError.BadRequest("Такая фраза отсутствует");
+
+				// изменяем фразу
+				db.query(`
+					UPDATE phrase SET caption='${newPhrase}'
+					WHERE caption='${oldPhrase}' RETURNING *;
+				`)
+					.then(resp => res.json(resp.rows[0]))
+					.catch(err => {
+						if (Number(err.code) === 23505) {
+							res.status(400).json({ message: "Такая фраза уже существует!" });
+						} else {
+							res.status(400).json({ message: "Ошибка при изменении." });
+						}
+					});
+			}
+		} catch (err) {
+			res.status(400).json({ message: err });
 		}
-
-		// после всех проверок достаём фразы для изменения в БД
-		const { oldPhrase, newPhrase } = req.body;
-
-		db.query(`
-			UPDATE phrase SET caption='${newPhrase}'
-			WHERE caption='${oldPhrase}' RETURNING *;
-		`)
-			.then(resp => res.json(resp.rows[0]))
-			.catch(err => {
-				if (Number(err.code) === 23505) {
-					res.status(400).json({ message: "Такая фраза уже существует!" });
-				} else {
-					res.status(400).json({ message: "Ошибка при изменении." });
-				}
-			});
 	}
 
-	getPhraseById(req, res) {
-		const { id } = req.params;
-		db.query(`SELECT * FROM phrase WHERE id=${id};`)
-			.then(resp => res.json(resp.rows))
-			.catch(err => res.json({ message: err }));
+	async getPhraseById(req, res) {
+		try {
+			const { isAccessValid } = await primaryCheckUser(req.cookies);
+			if (!isAccessValid.email) throw ApiError.UnathorizedError();
+
+			// получаем id юзера, по email из токена
+			const checkGetUser = await db.query(`
+				SELECT * FROM users
+				WHERE email = '${isAccessValid.email}';
+			`);
+
+			if (!checkGetUser.rowCount) throw ApiError.UnathorizedError();
+			const role = checkGetUser.rows[0].role;
+			const roleDescription = role === 1 ? 'admin' : role === 2 ? 'user' : 'unknown';
+
+			if (roleDescription === 'admin') {
+				const { id } = req.params;
+				db.query(`SELECT * FROM phrase WHERE id=${id};`)
+					.then(resp => res.json(resp.rows))
+					.catch(err => res.json({ message: err }));
+			}
+		} catch (err) {
+			res.status(400).json({ message: err });
+		}
 	}
 
 	getRandomPhrase(req, res) {
@@ -114,17 +157,32 @@ class PhraseController {
 	}
 
 	async getAllPhrases(req, res) {
-		const { isAccessValid } = await primaryCheckUser(req.cookies);
-		if (!isAccessValid.email) throw ApiError.UnathorizedError();
-
-		// после всех проверок достаём фразы в зависимости от прав пользователя
 		try {
-			const isPhrases = await db.query(`SELECT * FROM phrase ORDER BY caption;`);
-			const out = isPhrases.rows.map(item => item.caption);
+			const { isAccessValid } = await primaryCheckUser(req.cookies);
+			if (!isAccessValid.email) throw ApiError.UnathorizedError();
+
+			// получаем id юзера, по email из токена
+			const checkGetUser = await db.query(`
+				SELECT * FROM users
+				WHERE email = '${isAccessValid.email}';
+			`);
+
+			if (!checkGetUser.rowCount) throw ApiError.UnathorizedError();
+			const role = checkGetUser.rows[0].role;
+			const roleDescription = role === 1 ? 'admin' : role === 2 ? 'user' : 'unknown';
+
+			let phrasesData = [];
+			if (roleDescription === 'admin') {
+				// после всех проверок достаём фразы в зависимости от прав пользователя
+				const isPhrases = await db.query(`SELECT * FROM phrase ORDER BY caption;`);
+				phrasesData = isPhrases.rows.map(item => item.caption);
+			}
+
 			res.json(out);
 		} catch (err) {
 			res.status(400).json({ message: err });
 		}
+
 	}
 }
 
--- server/routers/phraseRouter.js
+++ server/routers/phraseRouter.js
@@ -5,12 +5,12 @@ import roleCheck from "../middlewares/roleCheck.js";
 const phraseRouter = Router();
 
 
-phraseRouter.post('/add', roleCheck(1), PhraseController.addPhrase);
-phraseRouter.post('/delete', roleCheck(1), PhraseController.deletePhrase);
-phraseRouter.put('/change', roleCheck(1), PhraseController.changePhrase);
-phraseRouter.get('/getbyid/:id', roleCheck(1), PhraseController.getPhraseById);
+phraseRouter.post('/add', PhraseController.addPhrase);
+phraseRouter.post('/delete', PhraseController.deletePhrase);
+phraseRouter.put('/change', PhraseController.changePhrase);
+phraseRouter.get('/getbyid/:id', PhraseController.getPhraseById);
 phraseRouter.get('/phrasernd', PhraseController.getRandomPhrase);
-phraseRouter.get('/phrases', roleCheck(1), PhraseController.getAllPhrases);
+phraseRouter.get('/phrases', PhraseController.getAllPhrases);
 
 
 export default phraseRouter;
