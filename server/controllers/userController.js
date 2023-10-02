2b47e37d5f04ff67c38b6c7a1ad7343b0abcbf16
CharlyChaplin <charlychaplin83@gmail.com>
2023-10-02 14:02:48 +0700

    ...

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -356,8 +356,8 @@ class UserController {
 			WHERE email = '${isAccessValid.email}';
 			`);
 			let userId = 0;
-			if (!getUser.rowCount) return res.status(200).json({ message: "HiUser not authorized" });
 			console.log("Hello");
+			if (!getUser.rowCount) return res.status(200).json({ message: "HiUser not authorized" });
 			userId = getUser.rows[0].id;
 			// получаем nickname юзера из дополнительной таблицы юзеров
 			const getUserData = await db.query(`
