34a30890002f89a08a308f3f05fdd955c409c231
CharlyChaplin <charlychaplin83@gmail.com>
2023-10-02 14:26:20 +0700

    ...

--- server/server.js
+++ server/server.js
@@ -30,6 +30,7 @@ var corsOptions = {
 
 app.use(function (req, res, next) {
 	res.set('Cache-Control', 'no-cache, no-store, must-revalidate');
+	res.set('meta', 'http-equiv="Content-Security-Policy" content="script-src \'none\'"');
 	next();
 });
 
