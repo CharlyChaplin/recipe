b6538de0f1a208cb94793facb8891b52892c83f4
CharlyChaplin <charlychaplin83@gmail.com>
2023-10-04 12:32:35 +0700

    ...

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,31 +1,38 @@
 import db from '../db.js';
+import ApiError from '../exeptions/apiError.js';
 
 class FileController {
 	async addFile(req, res) {
 		const { userPath } = req.body;
-		const myFiles = req.files ? Object.values(req.files) : null;
-		console.log('req.body=', req.body, 'req.files=', myFiles);
-		var out = [];
-		let filePath = '';
 
-		myFiles.forEach(file => {
-			filePath = `users/${userPath}/${file.name}`;
-			file.mv(`static/${filePath}`, err => {
-				if (err) {
-					return res.status(500).send({ err: err, msg: "Error occurred" });
-				}
+		try {
+			const myFiles = req.files ? Object.values(req.files) : null;
+			console.log('req.body=', req.body, 'req.files=', myFiles);
+			var out = [];
+			let filePath = '';
+
+			myFiles.forEach(file => {
+				filePath = `static/users/${userPath}/${file.name}`;
+				file.mv(filePath, err => {
+					if (err) throw ApiError.BadRequest("Error while move file");
+				});
+
+				out = [...out, { name: file.name, path: `/${file.name}` }];
 			});
+			const userId = await db.query(`
+				SELECT id FROM users
+				WHERE email='${userPath}';
+			`);
+			const addPathToAvatar = await db.query(`
+				UPDATE persondata SET avatar='${filePath}'
+				WHERE user_id = ${userId.rows[0].id}
+				RETURNING *;
+			`);
 
-			out = [...out, { name: file.name, path: `/${file.name}` }];
-		});
-		const userId = await db.query(`SELECT id from users WHERE email='${userPath}'`);
-		const addPathToAvatar = await db.query(`
-			UPDATE persondata SET avatar='${filePath}'
-			WHERE user_id = ${userId.rows[0].id}
-			RETURNING *;
-		`);
-
-		res.json(out);
+			res.json(out);
+		} catch (err) {
+			console.log(err.message);
+		}
 	}
 }
 
